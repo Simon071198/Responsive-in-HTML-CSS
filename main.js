d7c2e6fe12cd3e1ae8630bbd6a7db1c5edfc57d3
Simon Tampubolon <smntampubolon18@gmail.com>
2022-03-05 14:49:06 +0700

    Starter file

--- main.js
+++ main.js
@@ -26,7 +26,23 @@ var rellax = new Rellax('.rellax', {
 }) 
 
 // dark light mode
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+const body = document.querySelector('body');
+const button = document.querySelector('#darkbutton');
+function toggleDark() {
+    if (body.classList.contains('dark')) {
+    body.classList.remove('dark');
+    localStorage.setItem("theme", "light");
+    //button.innerHTML = "Turn on dark mode";
+    } else {
+    body.classList.add('dark');
+    localStorage.setItem("theme", "dark");
+    //button.innerHTML = "Turn off dark mode";
+    }
 }
+
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add('dark');
+    //button.innerHTML = "Turn off dark mode";
+    }
+
+document.querySelector('#darkbutton').addEventListener('click', toggleDark);
